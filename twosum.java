9edca2c568e43a55d5b1e89b1521e35c0fd33c52
andy975306 <50614848+andy975306@users.noreply.github.com>
2022-09-20 15:52:40 -0700

    Update twosum.java

--- twosum.java
+++ twosum.java
@@ -3,11 +3,11 @@ import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for (int i = 0; i < nums.length; ++i) {
-            if (map.containsKey(target - nums[i])) {
+        for (int i = 0; i < nums.length; ++i) {//go through all the integer once
+            if (map.containsKey(target - nums[i])) {//check if the current complement is in the hash table
                 return new int[] { map.get(target - nums[i]), i };
             }
-            map.put(nums[i], i);
+            map.put(nums[i], i);//Store the complement and the index in the hashtable.
         }
         return null;
     }
@@ -19,3 +19,5 @@ class Solution {
         System.out.println("Index[" + res[0] + "," + res[1] + "]");
     }
 }
+//time complexity is O(n) since we only go through the array once
+//space complexity is O(n) extra space for the hash table
